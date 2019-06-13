ad38a3d88690c84962a1cdbd029d8b2ce1a1079b
Cyril Leroux <cyril0leroux@gmail.com>
2019-06-13 19:38:26 +0200

    Added a filter to the networking engine to intercept network responses

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@
 // Global variables
 //
 
-var manifestUri =
-  'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
+const manifestUri = 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
 
 //
 // Main functions
@@ -33,7 +32,8 @@ function initPlayer() {
   window.player = player;
 
   // listenToAllEvents(player);
-  listenToSpecificEvents(player);
+  // listenToSpecificEvents(player);
+  bindNetworkCalls(player);
 
   // Load the stream
   player
@@ -157,6 +157,36 @@ function onEventManifestParsed(event) {
   } else {
     console.info('[STREAM_TYPE] VoD');
   }
+}
+
+function bindNetworkCalls(player) {
+
+  const networkingEngine = player.getNetworkingEngine();
+
+  const responseFilter = (responseType, request) => {
+
+    switch (responseType) {
+      case shaka.net.NetworkingEngine.RequestType.MANIFEST:
+        console.info('[MANIFEST_DETECTED]');
+        break;
+      case shaka.net.NetworkingEngine.RequestType.SEGMENT:
+        console.info('[SEGMENT_DETECTED]');
+        break;
+      case shaka.net.NetworkingEngine.RequestType.LICENSE:
+        console.info('[LICENSE_DETECTED]');
+        break;
+      case shaka.net.NetworkingEngine.RequestType.APP:
+        console.info('[APP_DETECTED]');
+        break;
+      case shaka.net.NetworkingEngine.RequestType.TIMING:
+        console.info('[TIMING_DETECTED]');
+        break;
+      default:
+        console.error('[UNKNOWN_TYPE_DETECTED]', responseType, request);
+    }
+  };
+
+  networkingEngine.registerResponseFilter(responseFilter);
 }
 
 //
